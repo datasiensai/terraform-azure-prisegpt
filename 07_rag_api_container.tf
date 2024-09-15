3c07d835fc8f68624cab50f4ba85a90969e4e5e2
Biju Krishnan <biju.krishnan@gmail.com>
2024-09-15 11:22:13 +0200

    added infrastructure_subnet_id

--- 07_rag_api_container.tf
+++ 07_rag_api_container.tf
@@ -3,6 +3,7 @@ resource "azurerm_container_app_environment" "rag_api_app" {
   location                          = var.location
   resource_group_name               = azurerm_resource_group.az_openai_rg.name
   internal_load_balancer_enabled    = true
+  infrastructure_subnet_id          = var.libre_app_virtual_network_subnet_id != null ? var.libre_app_virtual_network_subnet_id : azurerm_subnet.az_openai_subnet.id
   tags                              = var.tags
 }
 
