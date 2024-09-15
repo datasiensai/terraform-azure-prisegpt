868b823b8b20404a7eab22e7b86c1ca11ec823fe
Biju Krishnan <biju.krishnan@gmail.com>
2024-09-15 11:14:12 +0200

    changed var.rag_api_app_name

--- 07_rag_api_container.tf
+++ 07_rag_api_container.tf
@@ -1,21 +1,19 @@
 resource "azurerm_container_app_environment" "rag_api_app" {
-  name                              = "cae-${rag_api_app_name}"
+  name                              = "cae-${var.rag_api_app_name}"
   location                          = var.location
   resource_group_name               = azurerm_resource_group.az_openai_rg.name
   internal_load_balancer_enabled    = true
-  tags =  var.tags
+  tags                              = var.tags
 }
 
 resource "azurerm_container_app" "rag_api_app_name" {
-  name                         = "ca-${rag_api_app_name}"
+  name                         = "ca-${var.rag_api_app_name}"
   container_app_environment_id = azurerm_container_app_environment.rag_api_app.id
   resource_group_name          = azurerm_resource_group.az_openai_rg.name
   revision_mode                = "Single"
-  
 
- 
   registry {
-    server               = "ghcr.io"    
+    server = "ghcr.io"
   }
 
   ingress {
@@ -25,17 +23,14 @@ resource "azurerm_container_app" "rag_api_app_name" {
     traffic_weight {
       percentage = 100
     }
-
   }
 
   template {
     container {
-      name   = "${rag_api_app_name}"
+      name   = "${var.rag_api_app_name}"
       image  = "danny-avila/librechat-rag-api-dev:latest"
       cpu    = "1.0"
       memory = "2.0Gi"
     }
-  }  
-
-
+  }
 }
