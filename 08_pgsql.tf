7c39b07154155f60bcbb82fc213e9b52f8a553da
Biju Krishnan <biju.krishnan@gmail.com>
2024-09-28 13:45:42 +0200

    reset to an old commit

--- 08_pgsql.tf
+++ 08_pgsql.tf
@@ -6,7 +6,7 @@ resource "azurerm_postgresql_flexible_server" "default" {
   delegated_subnet_id    = azurerm_subnet.pgsql_subnet.id
   private_dns_zone_id    = azurerm_private_dns_zone.default.id
   administrator_login    = var.pgsql_administrator_login
-  administrator_password = var.pgsql_administrator_password
+  administrator_password = random_password.pgsql_password.result
   zone                   = "1"
   storage_mb             = var.pgsql_storage_mb
   sku_name               = var.pgsql_sku_name
@@ -22,4 +22,19 @@ resource "azurerm_postgresql_flexible_server_configuration" "default" {
   name      = "azure.extensions"
   server_id = azurerm_postgresql_flexible_server.default.id
   value     = join(",", var.database_extensions)
+}
+resource "random_password" "pgsql_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "azurerm_key_vault_secret" "pgsql_password" {
+  name         = "pgsql-admin-password"
+  value        = random_password.pgsql_password.result
+  key_vault_id = azurerm_key_vault.az_openai_kv.id
+
+  depends_on = [
+    azurerm_role_assignment.kv_role_assigment
+  ]
 }
